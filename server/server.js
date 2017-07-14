148fe12c97f9af3bc5d2ca0f75c5a10d4ce41c43
Jose Miranda <feu.joseiii@gmail.com>
2017-07-14 22:12:40 +0800

    Added installed test modules, created few test cases

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
-var bodyParser = require('bodyParser');
-var express = require('express');
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const bodyParser = require('body-parser');
+const express = require('express');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 //initial configurations
-var app = express();
-var port = process.env || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
+//middleware
+app.use(bodyParser.json());
 
+//routes
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+      text: req.body.text
+  });
+
+  todo.save().then((ret) => {
+    res.send(ret);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(port, () => {
-  return console.log(`Application is running at port ${port}`);
+  console.log(`Server is running on port ${port}`);;
 });
+
+module.exports = { app }
